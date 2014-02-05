fa28bd87ef0e6526a10d50b6718dc12f06cee4d7
Konstantin Solomatov <Konstantin.Solomatov@gmail.com>
2014-02-05 16:37:08 -0500

    update license

--- lambda/src/main/java/jetbrains/jetpad/lambda/LambdaDemo.java
+++ lambda/src/main/java/jetbrains/jetpad/lambda/LambdaDemo.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012-2014 JetBrains s.r.o
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package jetbrains.jetpad.lambda;
 
 import jetbrains.jetpad.cell.CellContainer;
@@ -8,14 +23,19 @@ import jetbrains.jetpad.projectional.util.RootController;
 public class LambdaDemo {
   private static Container createModel() {
     Container root = new Container();
-    LambdaExpr lambda = new LambdaExpr();
-    lambda.varName.set("x");
-
+    LambdaExpr l1 = new LambdaExpr();
+    l1.varName.set("x");
     VarExpr var = new VarExpr();
     var.name.set("x");
-    lambda.body.set(var);
+    l1.body.set(var);
 
-    root.expr.set(lambda);
+    LambdaExpr l2 = (LambdaExpr) l1.copy();
+
+    AppExpr app = new AppExpr();
+    app.fun.set(l1);
+    app.arg.set(l2);
+
+    root.expr.set(app);
     return root;
   }
 
