266b7768d7e9b6a8fdf2085ed84b71dd2c0fafe4
Konstantin Solomatov <Konstantin.Solomatov@gmail.com>
2014-02-12 21:56:03 -0500

    minor cleanup

--- grammar/src/main/java/jetbrains/jetpad/grammar/Grammar.java
+++ grammar/src/main/java/jetbrains/jetpad/grammar/Grammar.java
@@ -29,7 +29,7 @@ public final class Grammar {
 
   public Grammar() {
     myEnd = newTerminal("$");
-    myStart = newNonTerminal("S");
+    myStart = newNonTerminal("Start");
   }
 
   public NonTerminal getStart() {
