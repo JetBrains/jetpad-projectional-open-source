3a35650455993631f1ab07920d48571b9ba97f36
Konstantin Solomatov <Konstantin.Solomatov@gmail.com>
2014-02-18 22:54:26 -0500

    parse node property

---
+++ hybrid/src/main/java/jetbrains/jetpad/hybrid/parser/prettyprint/ParseNodeProperty.java
@@ -0,0 +1,20 @@
+package jetbrains.jetpad.hybrid.parser.prettyprint;
+
+public class ParseNodeProperty<ValueT> {
+  private String myName;
+  private ValueT myValue;
+
+  public ParseNodeProperty(String name, ValueT value) {
+    myName = name;
+    myValue = value;
+  }
+
+  public ValueT getDefaultValue() {
+    return myValue;
+  }
+
+  @Override
+  public String toString() {
+    return myName;
+  }
+}
