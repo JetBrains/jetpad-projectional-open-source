b508b512f97ce4eed8d60b0ee1af5f7cfc231d6e
Konstantin Solomatov <Konstantin.Solomatov@gmail.com>
2014-05-01 13:08:55 -0400

    work on animations

--- cell/src/main/java/jetbrains/jetpad/cell/animation/Animation.java
+++ cell/src/main/java/jetbrains/jetpad/cell/animation/Animation.java
@@ -2,6 +2,6 @@ package jetbrains.jetpad.cell.animation;
 
 public interface Animation {
   void stop();
-  void whenFinished(Runnable r);
-  void whenStopped(Runnable r);
+
+  void whenDone(Runnable r);
 }
--- cell/src/main/java/jetbrains/jetpad/cell/animation/DefaultAnimation.java
+++ cell/src/main/java/jetbrains/jetpad/cell/animation/DefaultAnimation.java
@@ -4,44 +4,29 @@ import jetbrains.jetpad.base.Handler;
 import jetbrains.jetpad.base.SimpleAsync;
 
 public abstract class DefaultAnimation implements Animation {
-  private SimpleAsync<Object> myFinish = new SimpleAsync<>();
-  private SimpleAsync<Object> myStop = new SimpleAsync<>();
-  private boolean myFinished;
-  private boolean myStopped;
+  private SimpleAsync<Object> myWhenDone = new SimpleAsync<>();
+  private boolean myDone;
 
   protected abstract void doStop();
 
+  public void done() {
+    if (myDone) {
+      throw new IllegalStateException();
+    }
+    myWhenDone.onSuccess(null);
+    myDone = true;
+  }
+
   @Override
   public void stop() {
-    if (myFinished || myStopped) {
-      throw new IllegalStateException();
-    }
     doStop();
-    myStop.success(null);
-    myStopped = true;
+    done();
   }
 
-  public void finish() {
-    if (myFinished || myStopped) {
-      throw new IllegalStateException();
-    }
-    myFinish.success(null);
-    myFinished = true;
-  }
 
   @Override
-  public void whenFinished(final Runnable r) {
-    myFinish.onSuccess(new Handler<Object>() {
-      @Override
-      public void handle(Object item) {
-        r.run();
-      }
-    });
-  }
-
-  @Override
-  public void whenStopped(final Runnable r) {
-    myStop.onSuccess(new Handler<Object>() {
+  public void whenDone(final Runnable r) {
+    myWhenDone.onSuccess(new Handler<Object>() {
       @Override
       public void handle(Object item) {
         r.run();
