4fbcd9a7e6346646d8bf47d3cad56d092d955703
Konstantin Solomatov <Konstantin.Solomatov@gmail.com>
2014-05-09 15:06:08 -0400

    refactored view to dom mapping

---
+++ view/src/main/java/jetbrains/jetpad/projectional/view/toGwt/ViewContainerToElementMapper.java
@@ -0,0 +1,449 @@
+package jetbrains.jetpad.projectional.view.toGwt;
+
+import com.google.common.base.Strings;
+import com.google.common.base.Supplier;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.query.client.Function;
+import com.google.gwt.query.client.GQuery;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Window;
+import jetbrains.jetpad.base.Handler;
+import jetbrains.jetpad.base.Registration;
+import jetbrains.jetpad.base.Value;
+import jetbrains.jetpad.base.animation.Animation;
+import jetbrains.jetpad.base.edt.EventDispatchThread;
+import jetbrains.jetpad.base.edt.JsEventDispatchThread;
+import jetbrains.jetpad.event.*;
+import jetbrains.jetpad.event.dom.ClipboardSupport;
+import jetbrains.jetpad.event.dom.EventTranslator;
+import jetbrains.jetpad.geometry.Rectangle;
+import jetbrains.jetpad.geometry.Vector;
+import jetbrains.jetpad.mapper.Mapper;
+import jetbrains.jetpad.mapper.MappingContext;
+import jetbrains.jetpad.mapper.Synchronizers;
+import jetbrains.jetpad.mapper.gwt.DomAnimations;
+import jetbrains.jetpad.mapper.gwt.GwtSynchronizers;
+import jetbrains.jetpad.model.event.CompositeRegistration;
+import jetbrains.jetpad.model.event.EventHandler;
+import jetbrains.jetpad.model.property.*;
+import jetbrains.jetpad.projectional.domUtil.Scrolling;
+import jetbrains.jetpad.projectional.domUtil.TextMetrics;
+import jetbrains.jetpad.projectional.domUtil.TextMetricsCalculator;
+import jetbrains.jetpad.projectional.view.TextView;
+import jetbrains.jetpad.projectional.view.View;
+import jetbrains.jetpad.projectional.view.ViewContainer;
+import jetbrains.jetpad.projectional.view.dom.DomView;
+import jetbrains.jetpad.projectional.view.spi.NullViewContainerPeer;
+import jetbrains.jetpad.projectional.view.spi.ViewContainerPeer;
+import jetbrains.jetpad.values.Font;
+
+import java.util.Collections;
+
+import static com.google.gwt.query.client.GQuery.$;
+
+public class ViewContainerToElementMapper extends Mapper<ViewContainer, Element> {
+  private Element myRootDiv = DOM.createDiv();
+  private ValueProperty<Rectangle> myVisibleArea = new ValueProperty<>();
+  private Property<Mapper<? extends View, ? extends Element>> myRootMapper = createChildProperty();
+  private View2DomContext myCtx = new View2DomContext() {
+    @Override
+    public ReadableProperty<Rectangle> visibleArea() {
+      return myVisibleArea;
+    }
+  };
+
+
+  public ViewContainerToElementMapper(ViewContainer source, Element target) {
+    super(source, target);
+
+    disablePopup(myRootDiv);
+    target.appendChild(myRootDiv);
+    myRootDiv.setTabIndex(0);
+
+    final Style rootDivStyle = myRootDiv.getStyle();
+    rootDivStyle.setPosition(Style.Position.RELATIVE);
+    rootDivStyle.setPadding(0, Style.Unit.PX);
+    rootDivStyle.setOverflow(Style.Overflow.VISIBLE);
+    rootDivStyle.setOutlineStyle(Style.OutlineStyle.NONE);
+  }
+
+  @Override
+  protected void registerSynchronizers(SynchronizersConfiguration conf) {
+    super.registerSynchronizers(conf);
+
+    conf.add(GwtSynchronizers.forRegistration(Window.addResizeHandler(new ResizeHandler() {
+      @Override
+      public void onResize(ResizeEvent event) {
+        update();
+      }
+    })));
+
+    conf.add(GwtSynchronizers.forRegistration(Window.addWindowScrollHandler(new Window.ScrollHandler() {
+      @Override
+      public void onWindowScroll(Window.ScrollEvent event) {
+        update();
+      }
+    })));
+
+    conf.add(Synchronizers.forProperty(getSource().root().bounds(), new WritableProperty<Rectangle>() {
+      @Override
+      public void set(Rectangle value) {
+        Vector bottomRight = value.origin.add(value.dimension);
+        myRootDiv.getStyle().setWidth(bottomRight.x, Style.Unit.PX);
+        myRootDiv.getStyle().setHeight(bottomRight.y, Style.Unit.PX);
+      }
+    }));
+
+    final Value<Boolean> pressed = new Value<>(false);
+    final Value<Boolean> pressedOutside = new Value<>(false);
+
+    conf.add(Synchronizers.forRegistration(new Supplier<Registration>() {
+      @Override
+      public Registration get() {
+        CompositeRegistration reg = new CompositeRegistration();
+
+        reg.add(eventRegistration(Event.ONMOUSEDOWN, $(myRootDiv).mousedown(new Function() {
+          @Override
+          public boolean f(Event e) {
+            myRootDiv.focus();
+            MouseEvent me = toMouseEvent(e);
+            if (isDomViewEvent(me)) return true;
+            pressed.set(true);
+            getSource().mousePressed(me);
+            pressedOutside.set(false);
+            return false;
+          }
+        })));
+        reg.add(eventRegistration(Event.ONMOUSEDOWN, $(Document.get()).mousedown(new Function() {
+          @Override
+          public boolean f(Event e) {
+            pressed.set(true);
+            MouseEvent evt = toMouseEvent(e);
+            if (!isContainerEvent(evt)) {
+              pressedOutside.set(true);
+            }
+            return true;
+          }
+        })));
+
+        reg.add(eventRegistration(Event.ONMOUSEUP, $(Document.get()).mouseup(new Function() {
+          @Override
+          public boolean f(Event e) {
+            pressed.set(false);
+            return true;
+          }
+        })));
+        reg.add(eventRegistration(Event.ONMOUSEUP, $(myRootDiv).mouseup(new Function() {
+          @Override
+          public boolean f(Event e) {
+            pressed.set(false);
+            MouseEvent me = toMouseEvent(e);
+            if (isDomViewEvent(me)) return true;
+            getSource().mouseReleased(me);
+            return false;
+          }
+        })));
+
+        reg.add(eventRegistration(Event.ONMOUSEMOVE, $(Document.get()).mousemove(new Function() {
+          @Override
+          public boolean f(Event e) {
+            MouseEvent evt = toMouseEvent(e);
+            if (pressed.get() && !pressedOutside.get()) {
+              getSource().mouseDragged(evt);
+            }
+            return true;
+          }
+        })));
+        reg.add(eventRegistration(Event.ONMOUSEMOVE, $(myRootDiv).mousemove(new Function() {
+          @Override
+          public boolean f(Event e) {
+            MouseEvent evt = toMouseEvent(e);
+            if (pressed.get() && !pressedOutside.get()) {
+              getSource().mouseDragged(evt);
+            } else {
+              getSource().mouseMoved(evt);
+            }
+            return true;
+          }
+        })));
+
+        reg.add(eventRegistration(Event.ONMOUSEOVER, $(myRootDiv).mouseenter(new Function() {
+          @Override
+          public boolean f(Event e) {
+            MouseEvent me = toMouseEvent(e);
+            getSource().mouseEntered(me);
+            return false;
+          }
+        })));
+        reg.add(eventRegistration(Event.ONMOUSEOUT, $(myRootDiv).mouseout(new Function() {
+          @Override
+          public boolean f(Event e) {
+            getSource().mouseLeft(toMouseEvent(e));
+            return false;
+          }
+        })));
+
+        final ClipboardSupport clipboardSupport = new ClipboardSupport(myRootDiv);
+        reg.add(eventRegistration(Event.ONKEYDOWN, $(myRootDiv).keydown(new Function() {
+          @Override
+          public boolean f(Event e) {
+            return EventTranslator.dispatchKeyPress(e, new Handler<KeyEvent>() {
+              @Override
+              public void handle(final KeyEvent e) {
+                if (e.is(Key.SPACE)) {
+                  getSource().keyPressed(e);
+                  getSource().keyTyped(new KeyEvent(Key.SPACE, ' ', Collections.<ModifierKey>emptySet()));
+                  return;
+                }
+
+                if (e.is(KeyStrokeSpecs.PASTE)) {
+                  clipboardSupport.pasteContent(new Handler<String>() {
+                    @Override
+                    public void handle(String text) {
+                      if (Strings.isNullOrEmpty(text)) {
+                        getSource().keyPressed(new KeyEvent(e.key(), e.keyChar(), e.modifiers()));
+                      } else {
+                        getSource().paste(new PasteEvent(new TextClipboardContent(text)));
+                      }
+                    }
+                  });
+                  return;
+                }
+
+                if (e.is(KeyStrokeSpecs.COPY) || e.is(KeyStrokeSpecs.CUT)) {
+                  CopyCutEvent copyEvent;
+                  if (e.is(KeyStrokeSpecs.CUT)) {
+                    getSource().cut(copyEvent = new CopyCutEvent(true));
+                  } else {
+                    getSource().copy(copyEvent = new CopyCutEvent(false));
+                  }
+                  ClipboardContent content = copyEvent.getResult();
+                  if (content != null) {
+                    clipboardSupport.copyContent(content);
+                  }
+                  return;
+                }
+
+                getSource().keyPressed(e);
+              }
+            });
+          }
+        })));
+        reg.add(eventRegistration(Event.ONKEYUP, $(myRootDiv).keyup(new Function() {
+          @Override
+          public boolean f(Event e) {
+            return EventTranslator.dispatchKeyRelease(e, new Handler<KeyEvent>() {
+              @Override
+              public void handle(KeyEvent e) {
+                getSource().keyReleased(e);
+              }
+            });
+          }
+        })));
+        reg.add(eventRegistration(Event.ONKEYPRESS, $(myRootDiv).keypress(new Function() {
+          @Override
+          public boolean f(Event e) {
+            return EventTranslator.dispatchKeyType(e, new Handler<KeyEvent>() {
+              @Override
+              public void handle(KeyEvent e) {
+                //Space is a special key in Chrome. We emulate its typing in keydown
+                if (e.keyChar() == ' ') return;
+                getSource().keyTyped(e);
+              }
+            });
+          }
+        })));
+
+        return reg;
+      }
+    }));
+  }
+
+  @Override
+  protected void onAttach(MappingContext ctx) {
+    super.onAttach(ctx);
+    update();
+
+    myRootMapper.set(ViewMapperFactory.factory(myCtx).createMapper(getSource().root()));
+    myRootDiv.appendChild(myRootMapper.get().getTarget());
+
+    TextMetrics metrics = TextMetricsCalculator.calculate(TextView.DEFAULT_FONT, "x");
+    final int baseLine = metrics.baseLine();
+    final int fontWidth = metrics.dimension().x;
+    final int fontHeight = metrics.dimension().y;
+
+    getSource().setPeer(new ViewContainerPeer() {
+      private Registration myReg;
+
+      @Override
+      public void attach(final ViewContainer container) {
+        myReg = container.root().valid().addHandler(new EventHandler<PropertyChangeEvent<Boolean>>() {
+          @Override
+          public void onEvent(PropertyChangeEvent<Boolean> event) {
+            Scheduler.get().scheduleFinally(new Scheduler.ScheduledCommand() {
+              @Override
+              public void execute() {
+                container.root().validate();
+              }
+            });
+          }
+        });
+
+        container.root().validate();
+      }
+
+      @Override
+      public void detach() {
+        myReg.remove();
+      }
+
+      @Override
+      public void repaint(View view) {
+      }
+
+      @Override
+      public Rectangle visibleRect() {
+        getSource().root().validate();
+        return getSource().root().bounds().get();
+      }
+
+      @Override
+      public void scrollTo(Rectangle rect, View view) {
+        Scrolling.scrollTo(rect, (Element) myRootMapper.get().getDescendantMapper(view).getTarget());
+      }
+
+      @Override
+      public void boundsChanged(View view, PropertyChangeEvent<Rectangle> change) {
+      }
+
+      @Override
+      public int textHeight(Font font) {
+        if (font.equals(TextView.DEFAULT_FONT)) {
+          return fontHeight;
+        } else {
+          return TextMetricsCalculator.calculateAprox(font, "x").dimension().y;
+        }
+      }
+
+      @Override
+      public int textBaseLine(Font font) {
+        if (font.equals(TextView.DEFAULT_FONT)) {
+          return baseLine;
+        } else {
+          return TextMetricsCalculator.calculateAprox(font, "x").baseLine();
+        }
+      }
+
+      @Override
+      public int textWidth(Font font, String text) {
+        if (font.equals(TextView.DEFAULT_FONT)) {
+          return text.length() * fontWidth;
+        } else {
+          return TextMetricsCalculator.calculateAprox(font, text).dimension().x;
+        }
+      }
+
+      @Override
+      public void requestFocus() {
+        myRootDiv.focus();
+      }
+
+      @Override
+      public EventDispatchThread getEdt() {
+        return JsEventDispatchThread.INSTANCE;
+      }
+
+      @Override
+      public Animation fadeIn(View view, int duration) {
+        return DomAnimations.fadeIn((Element) myRootMapper.get().getDescendantMapper(view).getTarget(), duration);
+      }
+
+      @Override
+      public Animation fadeOut(View view, int duration) {
+        return DomAnimations.fadeOut((Element) myRootMapper.get().getDescendantMapper(view).getTarget(), duration);
+      }
+
+      @Override
+      public Animation showSlide(View view, int duration) {
+        return DomAnimations.showSlide((Element) myRootMapper.get().getDescendantMapper(view).getTarget(), duration);
+      }
+
+      @Override
+      public Animation hideSlide(View view, int duration) {
+        return DomAnimations.hideSlide((Element) myRootMapper.get().getDescendantMapper(view).getTarget(), duration);
+      }
+    });
+  }
+
+  @Override
+  protected void onDetach() {
+    super.onDetach();
+    getSource().setPeer(new NullViewContainerPeer());
+  }
+
+  private void update() {
+    Rectangle newRect = new Rectangle(Window.getScrollLeft() - myRootDiv.getAbsoluteLeft(), Window.getScrollTop() - myRootDiv.getAbsoluteTop(), Window.getClientWidth(), Window.getClientHeight());
+    if (myVisibleArea.get() != null && myVisibleArea.get().contains(newRect)) return;
+    myVisibleArea.set(expand(newRect));
+  }
+
+  private Rectangle expand(Rectangle rect) {
+    return new Rectangle(
+      rect.origin.x, rect.origin.y - rect.dimension.y / 2,
+      rect.dimension.x, 2 * rect.dimension.y
+    );
+  }
+
+  private native void disablePopup(Element el) /*-{
+    el.oncontextmenu = function () {
+      return false;
+    }
+  }-*/;
+
+  private boolean isDomViewEvent(MouseEvent e) {
+    View targetView = getSource().root().viewAt(e.location());
+    if (targetView instanceof DomView) {
+      return true;
+    }
+    return false;
+  }
+
+
+  private MouseEvent toMouseEvent(Event e) {
+    int cx = e.getClientX();
+    int cy = e.getClientY();
+
+    int scrollLeft = Window.getScrollLeft();
+    int scrollTop = Window.getScrollTop();
+
+    int absoluteLeft = myRootDiv.getAbsoluteLeft();
+    int absoluteTop = myRootDiv.getAbsoluteTop();
+
+    int elScrollTop = myRootDiv.getScrollTop();
+    int elScrollLeft = myRootDiv.getScrollLeft();
+
+    int x = cx + scrollLeft - absoluteLeft + elScrollLeft;
+    int y = cy + scrollTop - absoluteTop + elScrollTop;
+
+    return new MouseEvent(x, y);
+  }
+
+  private Registration eventRegistration(final int event, final GQuery query) {
+    return new Registration() {
+      @Override
+      public void remove() {
+        query.unbind(event);
+      }
+    };
+  }
+
+  private boolean isContainerEvent(MouseEvent evt) {
+    return getSource().contentRoot().bounds().get().contains(evt.location());
+  }
+}
