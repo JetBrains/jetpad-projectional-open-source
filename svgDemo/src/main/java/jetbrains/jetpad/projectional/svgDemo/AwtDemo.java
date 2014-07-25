0798f74530b75b20e9823dd88ed7fc813ba6ddd0
Anton Samoylov <Anton.Samoylov@jetbrains.com>
2014-07-25 23:00:52 +0400

    add interactivity (mouse click) to svg AwtDemo

--- svgDemo/src/main/java/jetbrains/jetpad/projectional/svgDemo/AwtDemo.java
+++ svgDemo/src/main/java/jetbrains/jetpad/projectional/svgDemo/AwtDemo.java
@@ -3,9 +3,12 @@ package jetbrains.jetpad.projectional.svgDemo;
 import jetbrains.jetpad.projectional.svg.SvgRoot;
 import jetbrains.jetpad.projectional.svg.toAwt.SvgRootDocumentMapper;
 import org.apache.batik.swing.JSVGCanvas;
+import org.apache.batik.swing.svg.AbstractJSVGComponent;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class AwtDemo {
   public static void main(String[] args) {
@@ -13,12 +16,39 @@ public class AwtDemo {
     frame.setLayout(new BorderLayout());
 
     JSVGCanvas svgCanvas = new JSVGCanvas();
+    svgCanvas.setDocumentState(AbstractJSVGComponent.ALWAYS_DYNAMIC);
 
-    SvgRoot svgRoot = DemoModel.createModel();
+    final SvgRoot svgRoot = DemoModel.createModel();
     SvgRootDocumentMapper mapper = new SvgRootDocumentMapper(svgRoot, SvgRootDocumentMapper.createDoc());
     mapper.attachRoot();
 
     svgCanvas.setSVGDocument(mapper.getTarget());
+
+    svgCanvas.addMouseListener(new MouseListener() {
+      @Override
+      public void mouseClicked(MouseEvent e) {
+        DemoModel.addCircle(svgRoot, e.getX(), e.getY());
+      }
+
+      @Override
+      public void mousePressed(MouseEvent e) {
+      }
+
+      @Override
+      public void mouseReleased(MouseEvent e) {
+
+      }
+
+      @Override
+      public void mouseEntered(MouseEvent e) {
+
+      }
+
+      @Override
+      public void mouseExited(MouseEvent e) {
+
+      }
+    });
 
     frame.add(svgCanvas);
     frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
--- svgDemo/src/main/java/jetbrains/jetpad/projectional/svgDemo/DemoModel.java
+++ svgDemo/src/main/java/jetbrains/jetpad/projectional/svgDemo/DemoModel.java
@@ -3,6 +3,7 @@ package jetbrains.jetpad.projectional.svgDemo;
 import jetbrains.jetpad.projectional.svg.SvgEllipse;
 import jetbrains.jetpad.projectional.svg.SvgRect;
 import jetbrains.jetpad.projectional.svg.SvgRoot;
+import jetbrains.jetpad.values.Color;
 
 public class DemoModel {
   public static SvgRoot createModel() {
@@ -15,26 +16,37 @@ public class DemoModel {
     ellipse.cy.set(80.0);
     ellipse.rx.set(100.0);
     ellipse.ry.set(50.0);
-    ellipse.fill.set(jetbrains.jetpad.values.Color.YELLOW);
+    ellipse.fill.set(Color.YELLOW);
 
     SvgEllipse ellipse2 = new SvgEllipse();
     ellipse2.cx.set(250.0);
     ellipse2.cy.set(85.0);
     ellipse2.rx.set(40.0);
     ellipse2.ry.set(85.0);
-    ellipse2.fill.set(jetbrains.jetpad.values.Color.GREEN);
+    ellipse2.fill.set(Color.GREEN);
 
     SvgRect rect = new SvgRect();
     rect.x.set(180.0);
     rect.y.set(50.0);
     rect.width.set(80.0);
     rect.height.set(50.0);
-    rect.fill.set(jetbrains.jetpad.values.Color.RED);
+    rect.fill.set(Color.RED);
 
     svgRoot.elements.add(ellipse);
     svgRoot.elements.add(rect);
     svgRoot.elements.add(ellipse2);
 
     return svgRoot;
+  }
+
+  public static void addCircle(SvgRoot svgRoot, int x, int y) {
+    SvgEllipse circle = new SvgEllipse();
+    circle.cx.set( (double) x);
+    circle.cy.set( (double) y);
+    circle.rx.set(10.0);
+    circle.ry.set(10.0);
+    circle.fill.set(Color.BLACK);
+
+    svgRoot.elements.add(circle);
   }
 }
