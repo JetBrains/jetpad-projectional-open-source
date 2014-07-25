44c3ac8c99ce7dcae6e2ac79bda1223bb3a35653
Anton Samoylov <Anton.Samoylov@jetbrains.com>
2014-07-25 23:10:57 +0400

    add interactivity (mouse click) to svg gwt demo

--- svgDemo/src/main/java/jetbrains/jetpad/projectional/svgDemo/Demo.java
+++ svgDemo/src/main/java/jetbrains/jetpad/projectional/svgDemo/Demo.java
@@ -1,6 +1,8 @@
 package jetbrains.jetpad.projectional.svgDemo;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.*;
 import jetbrains.jetpad.projectional.svg.SvgRoot;
 import jetbrains.jetpad.projectional.svg.toDom.SvgRootMapper;
@@ -8,10 +10,17 @@ import org.vectomatic.dom.svg.OMSVGSVGElement;
 
 public class Demo implements EntryPoint {
   public void onModuleLoad() {
-    SvgRoot svgRoot = DemoModel.createModel();
+    final SvgRoot svgRoot = DemoModel.createModel();
 
     SvgRootMapper mapper = new SvgRootMapper(svgRoot, new OMSVGSVGElement());
     mapper.attachRoot();
+
+    mapper.getTarget().addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent clickEvent) {
+        DemoModel.addCircle(svgRoot, clickEvent.getX(), clickEvent.getY());
+      }
+    });
 
     RootPanel.get().getElement().appendChild(mapper.getTarget().getElement());
   }
