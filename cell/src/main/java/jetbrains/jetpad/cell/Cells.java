485a89f5a41bf8d63e841b5cf6503b52b317e6cb
Konstantin Solomatov <Konstantin.Solomatov@gmail.com>
2014-08-05 00:43:18 -0400

    optimization in findCell

--- cell/src/main/java/jetbrains/jetpad/cell/Cells.java
+++ cell/src/main/java/jetbrains/jetpad/cell/Cells.java
@@ -1,10 +1,11 @@
 package jetbrains.jetpad.cell;
 
+import jetbrains.jetpad.cell.indent.IndentCell;
 import jetbrains.jetpad.geometry.Vector;
 
 public class Cells {
   public static Cell findCell(Cell current, Vector loc) {
-    if (!current.getBounds().contains(loc)) return null;
+    if (!(current instanceof IndentCell) && !current.getBounds().contains(loc)) return null;
     for (Cell child : current.children()) {
       if (!child.visible().get()) continue;
       Cell result = findCell(child, loc);
