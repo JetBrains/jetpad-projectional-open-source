2d4e4e0c6d56f5746cbbe2aeeafa92b07880de21
Anton Samoylov <Anton.Samoylov@jetbrains.com>
2014-09-10 15:20:32 +0400

    svg: Attr to Attribute renames

--- svg/src/test/java/jetbrains/jetpad/projectional/svg/SvgAttributeTest.java
+++ svg/src/test/java/jetbrains/jetpad/projectional/svg/SvgAttributeTest.java
@@ -20,7 +20,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class SvgAttrTest {
+public class SvgAttributeTest {
   SvgEllipseElement element;
 
   @Before
--- svg/src/test/java/jetbrains/jetpad/projectional/svg/SvgClassAttributeTest.java
+++ svg/src/test/java/jetbrains/jetpad/projectional/svg/SvgClassAttributeTest.java
@@ -21,7 +21,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class SvgClassAttrTest {
+public class SvgClassAttributeTest {
   private static String cl = "class";
   private static String altCl = "alt-class";
 
