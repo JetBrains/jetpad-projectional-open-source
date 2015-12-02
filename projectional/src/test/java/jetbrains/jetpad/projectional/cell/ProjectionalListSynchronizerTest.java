JetBrains/jetpad-projectional-open-source

commit 598d2ecfa721994115d7a21d54873c01412affa8

Prepare observable list subclasses
All the subclasses of AbstractObservableList now have to implement three additional methods:

* checkSet
* beforeItemSet
* afterItemSet

cell/src/main/java/jetbrains/jetpad/cell/Cell.java
@@ -737,36 +737,59 @@ public abstract class Cell implements NavComposite<Cell>, HasVisibility, HasFocu
     @Override
     protected void beforeItemAdded(int index, Cell item) {
       onBeforeChildAdded(new CollectionItemEvent<>(null, item, index, CollectionItemEvent.EventType.ADD));
-      item.changeParent(Cell.this);
-      Cell prev = index == 0 ? null : get(index - 1);
-      Cell next = index == size() ? null : get(index);
-      if (prev != null) {
-        prev.myNext = item;
-      }
-      if (next != null) {
-        next.myPrev = item;
-      }
-      item.myNext = next;
-      item.myPrev = prev;
+      attachItem(item, index - 1, index);
+    }
+
+    @Override
+    protected void checkSet(int index, Cell oldItem, Cell newItem) {
+      checkAdd(index, newItem);
+      checkRemove(index, oldItem);
+    }
+
+    @Override
+    protected void beforeItemSet(int index, Cell oldItem, Cell newItem) {
+      onBeforeChildRemoved(new CollectionItemEvent<>(oldItem, null, index, CollectionItemEvent.EventType.REMOVE));
+      detachItem(oldItem);
+      onBeforeChildAdded(new CollectionItemEvent<>(null, newItem, index, CollectionItemEvent.EventType.ADD));
+      attachItem(newItem, index - 1, index + 1);
     }
 
     @Override
     protected void beforeItemRemoved(int index, Cell item) {
+      onBeforeChildRemoved(new CollectionItemEvent<>(item, null, index, CollectionItemEvent.EventType.REMOVE));
+      detachItem(item);
+    }
+
+    private void attachItem(Cell item, int prevIndex, int nextIndex) {
+      item.changeParent(Cell.this);
+
+      if (prevIndex >= 0 && prevIndex < size()) {
+        Cell prev = get(prevIndex);
+        prev.myNext = item;
+        item.myPrev = prev;
+      }
+      if (nextIndex >= 0 && nextIndex < size()) {
+        Cell next = get(nextIndex);
+        next.myPrev = item;
+        item.myNext = next;
+      }
+    }
+
+    private void detachItem(Cell item) {
       if (isAttached() && myContainer.focusedCell.get() != null && Composites.isDescendant(item, myContainer.focusedCell.get())) {
         myContainer.focusedCell.set(null);
       }
-      onBeforeChildRemoved(new CollectionItemEvent<>(item, null, index, CollectionItemEvent.EventType.REMOVE));
       item.changeParent(null);
-      Cell next = item.myNext;
-      Cell prev = item.myPrev;
-      if (next != null) {
-        next.myPrev = prev;
-      }
+      final Cell prev = item.myPrev;
+      final Cell next = item.myNext;
       if (prev != null) {
         prev.myNext = next;
       }
-      item.myNext = null;
+      if (next != null) {
+        next.myPrev = prev;
+      }
       item.myPrev = null;
+      item.myNext = null;
     }
   }
 

demo/src/main/java/jetbrains/jetpad/projectional/demo/diagramExpr/mapper/SubList.java
@@ -28,6 +28,13 @@ abstract class SubList<ItemT> extends ObservableArrayList<ItemT> {
   }
 
   @Override
+  protected void afterItemSet(int index, ItemT oldItem, ItemT newItem, boolean success) {
+    super.afterItemSet(index, oldItem, newItem, success);
+    getBaseList().remove(oldItem);
+    getBaseList().add(newItem);
+  }
+
+  @Override
   protected void afterItemRemoved(int index, ItemT item, boolean success) {
     super.afterItemRemoved(index, item, success);
     getBaseList().remove(item);

diagram/src/main/java/jetbrains/jetpad/projectional/diagram/util/SubList.java
@@ -28,6 +28,13 @@ public abstract class SubList<ItemT> extends ObservableArrayList<ItemT> {
   }
 
   @Override
+  protected void afterItemSet(int index, ItemT oldItem, ItemT newItem, boolean success) {
+    super.afterItemSet(index, oldItem, newItem, success);
+    getBaseList().remove(oldItem);
+    getBaseList().add(newItem);
+  }
+
+  @Override
   protected void afterItemRemoved(int index, ItemT item, boolean success) {
     super.afterItemRemoved(index, item, success);
     getBaseList().remove(item);

diagram/src/main/java/jetbrains/jetpad/projectional/diagram/view/DiagramView.java
@@ -85,6 +85,13 @@ public class DiagramView extends GroupView {
     }
 
     @Override
+    protected void afterItemSet(int index, Connection oldItem, Connection newItem, boolean success) {
+      super.afterItemSet(index, oldItem, newItem, success);
+      myConnectionView.children().remove(oldItem.view());
+      myConnectionView.children().add(newItem.view());
+    }
+
+    @Override
     protected void afterItemRemoved(int index, Connection item, boolean success) {
       super.afterItemRemoved(index, item, success);
       myConnectionView.children().remove(item.view());

svg/src/main/java/jetbrains/jetpad/projectional/svg/SvgNode.java
@@ -80,6 +80,15 @@ public abstract class SvgNode extends HasParent<SvgNode, SvgNode> {
     }
 
     @Override
+    protected void beforeItemSet(int index, SvgNode oldItem, SvgNode newItem) {
+      if (isAttached()) {
+        oldItem.detach();
+        newItem.attach(container());
+      }
+      super.beforeItemSet(index, oldItem, newItem);
+    }
+
+    @Override
     protected void beforeItemRemoved(int index, SvgNode node) {
       if (isAttached()) {
         node.detach();

view/src/main/java/jetbrains/jetpad/projectional/view/View.java
@@ -806,6 +806,12 @@ public abstract class View implements NavComposite<View>, HasFocusability, HasVi
     }
 
     @Override
+    protected void beforeItemSet(int index, View oldItem, View newItem) {
+      beforeItemRemoved(index, oldItem);
+      beforeItemAdded(index, newItem);
+    }
+
+    @Override
     protected void beforeItemRemoved(int index, View item) {
       if (isAttached()) {
         item.detach();

commit 3a9f4352a919d859303c707355f335adbf9e1758

Fix deletion in indent cells.
The bug was introduced in 598d2ec.

cell/src/main/java/jetbrains/jetpad/cell/Cell.java
@@ -736,8 +736,7 @@ public abstract class Cell implements NavComposite<Cell>, HasVisibility, HasFocu
 
     @Override
     protected void beforeItemAdded(int index, Cell item) {
-      onBeforeChildAdded(new CollectionItemEvent<>(null, item, index, CollectionItemEvent.EventType.ADD));
-      attachItem(item, index - 1, index);
+      attachItem(item, index - 1, index, index);
     }
 
     @Override
@@ -748,19 +747,18 @@ public abstract class Cell implements NavComposite<Cell>, HasVisibility, HasFocu
 
     @Override
     protected void beforeItemSet(int index, Cell oldItem, Cell newItem) {
-      onBeforeChildRemoved(new CollectionItemEvent<>(oldItem, null, index, CollectionItemEvent.EventType.REMOVE));
-      detachItem(oldItem);
-      onBeforeChildAdded(new CollectionItemEvent<>(null, newItem, index, CollectionItemEvent.EventType.ADD));
-      attachItem(newItem, index - 1, index + 1);
+      detachItem(oldItem, index);
+      attachItem(newItem, index - 1, index, index + 1);
     }
 
     @Override
     protected void beforeItemRemoved(int index, Cell item) {
-      onBeforeChildRemoved(new CollectionItemEvent<>(item, null, index, CollectionItemEvent.EventType.REMOVE));
-      detachItem(item);
+      detachItem(item, index);
     }
 
-    private void attachItem(Cell item, int prevIndex, int nextIndex) {
+    private void attachItem(Cell item, int prevIndex, int index, int nextIndex) {
+      onBeforeChildAdded(new CollectionItemEvent<>(null, item, index, CollectionItemEvent.EventType.ADD));
+
       item.changeParent(Cell.this);
 
       if (prevIndex >= 0 && prevIndex < size()) {
@@ -775,10 +773,13 @@ public abstract class Cell implements NavComposite<Cell>, HasVisibility, HasFocu
       }
     }
 
-    private void detachItem(Cell item) {
+    private void detachItem(Cell item, int index) {
       if (isAttached() && myContainer.focusedCell.get() != null && Composites.isDescendant(item, myContainer.focusedCell.get())) {
         myContainer.focusedCell.set(null);
       }
+
+      onBeforeChildRemoved(new CollectionItemEvent<>(item, null, index, CollectionItemEvent.EventType.REMOVE));
+
       item.changeParent(null);
       final Cell prev = item.myPrev;
       final Cell next = item.myNext;

projectional/src/test/java/jetbrains/jetpad/projectional/cell/ProjectionalListSynchronizerTest.java
@@ -20,6 +20,7 @@ import com.google.common.base.Supplier;
 import jetbrains.jetpad.base.*;
 import jetbrains.jetpad.cell.*;
 import jetbrains.jetpad.cell.action.CellActions;
+import jetbrains.jetpad.cell.indent.IndentCell;
 import jetbrains.jetpad.cell.position.PositionHandler;
 import jetbrains.jetpad.cell.text.TextEditing;
 import jetbrains.jetpad.cell.trait.CellTrait;
@@ -263,6 +264,20 @@ public class ProjectionalListSynchronizerTest extends EditingTestCase {
     escape();
 
     assertEquals(1, container.children.size());
+  }
+
+  @Test
+  public void deleteSelectionInsideIndent() {
+    CompositeChild cc = new CompositeChild();
+    cc.children.add(new IndentChild());
+    container.children.add(cc);
+
+    CellActions.toFirstFocusable(getChild(0)).run();
+
+    backspace();
+    backspace();
+
+    assertEquals(0, cc.children.size());
   }
 
   @Test
@@ -969,6 +984,10 @@ public class ProjectionalListSynchronizerTest extends EditingTestCase {
           return new DeleteEatingChildMapper((DeleteEatingChild) source);
         }
 
+        if (source instanceof IndentChild) {
+          return new IndentChildMapper((IndentChild) source);
+        }
+
         return null;
       }
     };
@@ -1074,6 +1093,9 @@ public class ProjectionalListSynchronizerTest extends EditingTestCase {
   private class DeleteEatingChild extends Child {
   }
 
+  private class IndentChild extends Child {
+  }
+
   private class ContainerMapper extends Mapper<Container, ContainerCell> {
     private ProjectionalRoleSynchronizer<Object, Child> mySynchronizer;
 
@@ -1162,7 +1184,7 @@ public class ProjectionalListSynchronizerTest extends EditingTestCase {
     private CompositeChildMapper(CompositeChild source) {
       super(source, new HorizontalCell());
       getTarget().children().add(createInvisible());
-      getTarget().children().add(new HorizontalCell());
+      getTarget().children().add(new IndentCell());
       getTarget().children().add(createInvisible());
       getTarget().visible().set(true);
     }
@@ -1232,7 +1254,6 @@ public class ProjectionalListSynchronizerTest extends EditingTestCase {
     }
   }
 
-
   private class DeleteEatingChildMapper extends Mapper<DeleteEatingChild, TextCell> {
     public DeleteEatingChildMapper(DeleteEatingChild source) {
       super(source, CellFactory.label("aaaa"));
@@ -1250,6 +1271,16 @@ public class ProjectionalListSynchronizerTest extends EditingTestCase {
           super.onKeyPressed(cell, event);
         }
       });
+    }
+  }
+
+  private class IndentChildMapper extends Mapper<IndentChild, IndentCell> {
+    public IndentChildMapper(IndentChild source) {
+      super(source, CellFactory.indent());
+      Cell l = CellFactory.label(">>");
+      getTarget().children().add(l);
+
+      l.focusable().set(true);
     }
   }
 }