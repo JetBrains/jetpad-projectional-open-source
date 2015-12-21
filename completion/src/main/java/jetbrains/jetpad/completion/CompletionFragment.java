ba770c18c2eda2c1dffcdebbc3424c07a7a044d6
Ekaterina Frolova <ekaterina.frolova@jetbrains.com>
2015-12-21 20:01:36 +0300

    delete unused interface

--- completion/src/main/java/jetbrains/jetpad/completion/CompletionFragment.java
+++
@@ -1,9 +0,0 @@
-package jetbrains.jetpad.completion;
-
-import jetbrains.jetpad.base.Async;
-
-import java.util.List;
-
-public interface CompletionFragment {
-  Async<List<CompletionItem>> get(CompletionParameters params);
-}
