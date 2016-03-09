5dc0c19293d48802fdda0059eb88866798ebf7c2
Ekaterina Frolova <ekaterina.frolova@jetbrains.com>
2016-03-09 08:58:34 +0300

    get flexbox back

--- cell/src/main/java/jetbrains/jetpad/cell/toDom/IndentRootCellMapper.java
+++ cell/src/main/java/jetbrains/jetpad/cell/toDom/IndentRootCellMapper.java
@@ -34,6 +34,7 @@ import jetbrains.jetpad.cell.mappersUtil.CounterUtil;
 import jetbrains.jetpad.cell.mappersUtil.PopupManager;
 import jetbrains.jetpad.mapper.Mapper;
 import jetbrains.jetpad.mapper.MappingContext;
+import jetbrains.jetpad.mapper.gwt.DomUtil;
 import jetbrains.jetpad.model.collections.CollectionItemEvent;
 import jetbrains.jetpad.model.property.PropertyChangeEvent;
 import jetbrains.jetpad.projectional.domUtil.DomTextEditor;
@@ -96,7 +97,7 @@ class IndentRootCellMapper extends BaseCellMapper<IndentCell> {
         @Override
         public List<Node> children(Node item) {
           if (item instanceof Element) {
-            return divWrappedElementChildren((Element) item);
+            return DomUtil.elementChildren((Element) item);
           } else {
             throw new IllegalStateException();
           }
