5a135014a576fc860dae7cc2ebd0fb0733dbc93a
Alexey Zlobin <alexey.zlobin@jetbrains.com>
2016-03-11 17:47:50 +0300

    HybridProperty with default implementation

---
+++ hybrid/src/main/java/jetbrains/jetpad/hybrid/util/HybridPropertyImpl.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2012-2016 JetBrains s.r.o
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.jetpad.hybrid.util;
+
+import jetbrains.jetpad.base.Registration;
+import jetbrains.jetpad.hybrid.HybridProperty;
+import jetbrains.jetpad.hybrid.parser.Parser;
+import jetbrains.jetpad.hybrid.parser.ParsingContext;
+import jetbrains.jetpad.hybrid.parser.Token;
+import jetbrains.jetpad.model.collections.CollectionItemEvent;
+import jetbrains.jetpad.model.collections.list.ObservableList;
+import jetbrains.jetpad.model.event.EventHandler;
+import jetbrains.jetpad.model.property.BaseDerivedProperty;
+
+public class HybridPropertyImpl<ModelT> extends BaseDerivedProperty<ModelT> implements HybridProperty<ModelT> {
+
+  private final Parser<ModelT> myParser;
+  private final ObservableList<Token> myTokens;
+
+  private Registration myRegistration;
+
+  public HybridPropertyImpl(Parser<ModelT> parser, ObservableList<Token> tokens) {
+    super(parser.parse(new ParsingContext(tokens)));
+    myParser = parser;
+    myTokens = tokens;
+  }
+
+  @Override
+  protected void doAddListeners() {
+    myRegistration = myTokens.addHandler(new EventHandler<CollectionItemEvent<? extends Token>>() {
+      @Override
+      public void onEvent(CollectionItemEvent<? extends Token> event) {
+        somethingChanged();
+      }
+    });
+  }
+
+  @Override
+  protected void doRemoveListeners() {
+    myRegistration.remove();
+  }
+
+  @Override
+  protected ModelT doGet() {
+    return myParser.parse(new ParsingContext(myTokens));
+  }
+
+  @Override
+  public ObservableList<Token> source() {
+    return myTokens;
+  }
+}
---
+++ hybrid/src/test/java/jetbrains/jetpad/hybrid/util/HybridPropertyImplTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2012-2016 JetBrains s.r.o
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.jetpad.hybrid.util;
+
+import jetbrains.jetpad.hybrid.HybridProperty;
+import jetbrains.jetpad.hybrid.parser.ErrorToken;
+import jetbrains.jetpad.hybrid.parser.IdentifierToken;
+import jetbrains.jetpad.hybrid.parser.Token;
+import jetbrains.jetpad.hybrid.testapp.mapper.ExprHybridEditorSpec;
+import jetbrains.jetpad.hybrid.testapp.mapper.Tokens;
+import jetbrains.jetpad.hybrid.testapp.model.Expr;
+import jetbrains.jetpad.hybrid.testapp.model.PlusExpr;
+import jetbrains.jetpad.model.collections.list.ObservableArrayList;
+import jetbrains.jetpad.model.collections.list.ObservableList;
+import jetbrains.jetpad.test.BaseTestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HybridPropertyImplTest extends BaseTestCase {
+
+  private ObservableList<Token> myTokens;
+  private HybridProperty<Expr> myProp;
+
+  @Before
+  public void init() {
+    myTokens = new ObservableArrayList<>();
+    myProp = new HybridPropertyImpl<>(new ExprHybridEditorSpec().getParser(), myTokens);
+  }
+
+  @Test
+  public void empty() {
+    assertNull(myProp.get());
+  }
+
+  @Test
+  public void tokenError() {
+    myTokens.add(new ErrorToken("something wrong"));
+    assertNull(myProp.get());
+  }
+
+  @Test
+  public void parserError() {
+    myTokens.add(Tokens.PLUS);
+    assertNull(myProp.get());
+  }
+
+  @Test
+  public void invalidate() {
+    myTokens.add(new IdentifierToken("a"));
+    myTokens.add(Tokens.PLUS);
+    myTokens.add(new IdentifierToken("b"));
+    assertTrue(myProp.get() instanceof PlusExpr);
+
+    myTokens.remove(2);
+    assertNull(myProp.get());
+  }
+
+  @Test
+  public void validate() {
+    myTokens.add(new IdentifierToken("a"));
+    myTokens.add(Tokens.PLUS);
+    assertNull(myProp.get());
+
+    myTokens.add(new IdentifierToken("b"));
+    assertTrue(myProp.get() instanceof PlusExpr);
+  }
+}
