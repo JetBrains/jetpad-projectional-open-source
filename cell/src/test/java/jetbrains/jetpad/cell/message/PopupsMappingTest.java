fc4fea5d73ebef2c17c20591bb9792d374d2bc58
Ekaterina Frolova <ekaterina.frolova@jetbrains.com>
2016-03-14 16:41:15 +0300

    fix NPE when cell with popup is attached to IndentCell

--- cell/src/test/java/jetbrains/jetpad/cell/message/PopupsMappingTest.java
+++ cell/src/test/java/jetbrains/jetpad/cell/message/PopupsMappingTest.java
@@ -16,28 +16,74 @@
 package jetbrains.jetpad.cell.message;
 
 import jetbrains.jetpad.cell.Cell;
+import jetbrains.jetpad.cell.CellContainer;
 import jetbrains.jetpad.cell.HorizontalCell;
 import jetbrains.jetpad.cell.TextCell;
+import jetbrains.jetpad.cell.indent.IndentCell;
+import jetbrains.jetpad.cell.toView.CellContainerToViewMapper;
+import jetbrains.jetpad.projectional.view.ViewContainer;
+import jetbrains.jetpad.test.BaseTestCase;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 
-public class PopupsMappingTest extends MessageControllerTestCase {
+public class PopupsMappingTest extends BaseTestCase {
+  private CellContainer cellContainer;
+
+  @Before
+  public void init() {
+    cellContainer = new CellContainer();
+    MessageController.install(cellContainer);
+  }
 
   @Test
   public void setPopupAfterMapping() {
+    setPopupAfterMapping(new HorizontalCell());
+  }
+
+  @Test
+  public void setPopupAfterIndentMapping() {
+    setPopupAfterMapping(new IndentCell());
+  }
+
+  private void setPopupAfterMapping(Cell container) {
+    HorizontalCell cell = new HorizontalCell();
+    container.children().add(cell);
+    cellContainer.root.children().add(container);
+    CellContainerToViewMapper mapper = mapContainer();
+
     TextCell popup = new TextCell("popup");
     cell.bottomPopup().set(popup);
-    assertNotNull(getContainerMapper().getDescendantMapper(popup));
+    assertNotNull(mapper.getDescendantMapper(popup));
   }
 
   @Test
   public void attachCellWithPopup() {
-    Cell child = new HorizontalCell();
-    TextCell popup = new TextCell("popup");
-    child.bottomPopup().set(popup);
+    attachCellWithPopup(new HorizontalCell());
+  }
 
-    cell.children().add(child);
-    assertNotNull(getContainerMapper().getDescendantMapper(popup));
+  @Test
+  public void attachCellWithPopupToIndent() {
+    attachCellWithPopup(new IndentCell());
+  }
+
+  private void attachCellWithPopup(Cell container) {
+    cellContainer.root.children().add(container);
+    CellContainerToViewMapper mapper = mapContainer();
+
+    HorizontalCell cell = new HorizontalCell();
+    TextCell popup = new TextCell("popup");
+    cell.bottomPopup().set(popup);
+    container.children().add(cell);
+    assertNotNull(mapper.getDescendantMapper(popup));
+  }
+
+  private CellContainerToViewMapper mapContainer() {
+    ViewContainer viewContainer = new ViewContainer();
+    CellContainerToViewMapper mapper = new CellContainerToViewMapper(
+        cellContainer, viewContainer.root(), viewContainer.contentRoot(), viewContainer.decorationRoot());
+    mapper.attachRoot();
+    return mapper;
   }
 }
